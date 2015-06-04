62e9ab43ba3fd6ef47ff786740efd3bc37b35775
Peter Marton <martonpe@secretsaucepartners.com>
2015-06-04 11:53:36 +0200

    Remove commented out code from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
 require 'avro2kafka'
 
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each {|f| require f }
-
-# RSpec.configure do |config|
-#   config.after(:all) do
-#     Dir["./spec/support/*.avro", "./spec/support/*.bad*"].each do |file|
-#       File.delete(file)
-#     end
-#   end
-# end
